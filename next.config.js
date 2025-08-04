377a3922185efb495622ddbe02b856bdde4140d6
Yakubjanov Ulug‘bek <yoqubjonov229@gmail.com>
2025-08-04 12:18:26 +0500

    Fix all

--- next.config.js
+++ next.config.js
@@ -10,7 +10,7 @@ const nextConfig = {
     domains: ["t.me", "cdn.telegram.org"],
     unoptimized: true,
   },
-  assetPrefix: process.env.NODE_ENV === "production" ? "/telegram-chat-webapp" : "",
+  // assetPrefix: process.env.NODE_ENV === "production" ? "/telegram-chat-webapp" : "",
   trailingSlash: true,
   output: "export",
 }
